4413e216d4c6b28ea824bd5f89e9cc5d43c85777
Jinkoo Kim <jinkoo2@hotmail.com>
2021-02-17 17:13:55 -0500

    added env variables for node-app

--- node-app/index.js
+++ node-app/index.js
@@ -7,10 +7,21 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 
+const DB_USER_ID = process.env.DB_USER_ID || "dbuser";
+const DB_USER_PW = process.env.DB_USER_PW || "dbpw";
+const DB_SERVER = process.env.DB_SERVER || "mongodb";
+const DB_NAME = process.env.DB_NAME || "mydb";
+const DB_PORT = process.env.DB_PORT || 27017;
+
+console.log('DB_USER_ID='+DB_USER_ID)
+console.log('DB_SERVER='+DB_SERVER)
+console.log('DB_NAME='+DB_NAME)
+console.log('DB_PORT='+DB_PORT)
+
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://dbuser:dbpw@mongodb:27017/mydb',
+    `mongodb://${DB_USER_ID}:${DB_USER_PW}@${DB_SERVER}:${DB_PORT}/${DB_NAME}`,
     { useNewUrlParser: true}
   )
   .then(() => console.log('MongoDB Connected'))
@@ -34,4 +45,4 @@ app.post('/item/add', (req, res) => {
 
 const port = 3000;
 
-app.listen(port, () => console.log('Server running...'));
+app.listen(port, () => console.log('Server running at port '+port));
